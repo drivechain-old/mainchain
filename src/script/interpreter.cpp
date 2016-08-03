4a6d1859c4c52be9857f12808ce980d986399de3
CryptAxe <cryptaxe@gmail.com>
2016-08-03 16:41:08 -0700

    Revert to a5912daffbe8469c...

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -233,10 +233,6 @@ bool static CheckMinimalPush(const valtype& data, opcodetype opcode) {
 
 bool EvalScript(vector<vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, ScriptError* serror)
 {
-    uint256 id;
-    sidechainSidechain sidechain;
-    psidechaintree->GetSidechain(id, sidechain);
-
     static const CScriptNum bnZero(0);
     static const CScriptNum bnOne(1);
     static const CScriptNum bnFalse(0);
